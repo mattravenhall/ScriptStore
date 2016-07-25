f7928c9e9cba599f4faccfe0ace6bf2c44edfddc
Matt Ravenhall <m.ravenhall@live.co.uk>
2016-07-25 16:09:42 +0100

    General improvements
    NB. This script is currently not fit for purpose. The core stats works, but the pipeline doesn't.

--- getFst_Nei_Wright.r
+++ getFst_Nei_Wright.r
@@ -1,8 +1,10 @@
+## Under Construction ##
+# 25 July 2016
+
 # Calculate Fst
 # Input data: Matrix, SNP per row, Sample per column (with a population prefix)
 # Allele calls: Major as 0, minor as 1, mixed allowed
 # Populations must have a prefix which matches given population IDs
-# 14 Mar 2016
 
 # Resolve NA and -Inf issues!
 
@@ -21,7 +23,6 @@ if (FALSE) {
 }
 
 # Get A and n from input df
-
 calcStats <- function(inData, pops) {
   # Get A and n arrays from input data
   out.A <- c()
@@ -39,7 +40,7 @@ calcStats <- function(inData, pops) {
 }
 
 calcFstWright <- function(A, n) {
-  # Calculates Fst for one SNP
+  # Calculates weighted Wright Fst for one SNP
   # A = array of allele A frequencies for each population
   # n = array of population sizes in same order as A
 
@@ -54,7 +55,7 @@ calcFstWright <- function(A, n) {
 }
 
 calcFstNei <- function(A) {
-  # Calculates Fst estimation for one SNP
+  # Calculates Nei Fst estimation (Gst) for one SNP
   # A = array of allele A frequencies for each population
 
   Hs = sum(2*A*(1-A)) / length(A)
@@ -68,6 +69,7 @@ calcFst <- function(inData, pops, groups=NULL, pairwise=T, Nei=T) {
   # inData = input SNP table, as data.frame
   # pops = array of sub-population ID prefixes
   # groups = list of groups that those sub-pops belong to
+  groupwise = !pairwise
 
   if (pairwise) {
     # iterate pairs of populations (ie. Malawi v Tanzania)
@@ -75,26 +77,53 @@ calcFst <- function(inData, pops, groups=NULL, pairwise=T, Nei=T) {
     # Calculate pairs of populations
     pairs <- combn(pops,2)
 
+    # Calc stats for each population
+    all.A <- data.frame(SNP=1:dim(inData)[1])
+    all.n <- data.frame(SNP=1:dim(inData)[1])
+
+    for (i in pops) {
+      all.A[i] <- unlist(calcStats(inData,i)[1])
+      all.n[i] <- unlist(calcStats(inData,i)[2])
+    }
+
     # For each pair
+    outdf <- data.frame(t(pairs)); names(outdf) <- c('PopA','PopB')
     for (i in 1:dim(pairs)[2]) {
+      stats <- calcStats(inData,pairs[,i])
 
-      stats <- calcStats(pairs[inData,i])
-
-      A <- data.frame(stats[1])
+      #A <- data.frame(stats[1])
       n <- unlist(stats[2])
+      print(n)
+
+      A <- data.frame(all.A[pairs[1,1]],all.A[pairs[2,1]])
+      n <- data.frame(all.n[pairs[1,1]],all.n[pairs[2,1]])
+      print(n)
 
       if (Nei) { Fst <- apply(A, 1, calcFstNei) }
       else { Fst <- apply(A, 1, calcFstWright, n=n) } # e.g. if two, pairwise
 
       output <- data.frame(PopA=pairs[1,],PopB=pairs[2,],Fst,stringsAsFactors=F)
-
+      outdf[paste('Fst_',i,sep='')] <- output$Fst #<- cbind(outdf,Fst=output$Fst)
     }
-
-    values <- combn(inData[1,],2,sum) # replace with array fst call
-    output <- data.frame(PopA=pairs[1,],PopB=pairs[2,],values,stringsAsFactors=F)
+    print(outdf)
+  #  values <- combn(inData[1,],2,sum) # replace with array fst call
+  #  output <- data.frame(PopA=pairs[1,],PopB=pairs[2,],values,stringsAsFactors=F)
     # call Fst from pairs columns
+  }
+  # NB: No non-pairwise implementation
+
+  if (groupwise) {
+    # For 3+ populations at once
+    stats <- calcStats(inData, pops)
+
+    A <- data.frame(stats[1])
+    n <- unlist(stats[2])
+
+    if (Nei) { Fst <- apply(A, 1, calcFstNei) }
+    else { Fst <- apply(A, 1, calcFstWright, n=n) }
+
+    output <- data.frame()
   }
 }
 
-
-calcFst(dat, c('wAFR','eAFR','cAFR','sASI','seASI'))
+#calcFst(dat, c('wAFR','eAFR','cAFR','sASI','seASI'))
